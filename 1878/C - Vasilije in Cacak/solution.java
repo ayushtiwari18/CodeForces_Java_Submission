61a0623cc8aee5f5ceb7f5a07324eeddd7ed3afe
Ayush Tiwari <90121562+ayushtiwari18@users.noreply.github.com>
2025-07-13 00:20:17 +0530

    Add Vasilije in Cacak [C] from Codeforces

--- 1878/C - Vasilije in Cacak/solution.java
+++ 1878/C - Vasilije in Cacak/solution.java
@@ -3,23 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
- 
-        int t = sc.nextInt(); // Number of test cases
- 
+        int t = sc.nextInt(); // number of test cases
         while (t-- > 0) {
             long n = sc.nextLong();
             long k = sc.nextLong();
             long x = sc.nextLong();
  
-            // Minimum possible sum with k distinct smallest numbers
-            long sum1 = (k * (k + 1)) / 2;
- 
-            // Total sum of first n numbers
+            long sum1 = (k * (k + 1)) / 2; // smallest k distinct numbers: 1+2+...+k
             long total = (n * (n + 1)) / 2;
- 
-            // Maximum possible sum with k distinct largest numbers
             long tmp = n - k;
-            long sum2 = total - (tmp * (tmp + 1)) / 2;
+            long sum2 = total - (tmp * (tmp + 1)) / 2; // largest k distinct numbers: n+(n-1)+...+(n-k+1)
  
             if (x >= sum1 && x <= sum2) {
                 System.out.println("YES");
@@ -27,7 +20,6 @@ public class Main {
                 System.out.println("NO");
             }
         }
- 
         sc.close();
     }
 }
