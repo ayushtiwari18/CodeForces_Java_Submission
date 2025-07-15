0e9684b82eaca15ccdec87c1e52ac15af524c350
Ayush Tiwari <90121562+ayushtiwari18@users.noreply.github.com>
2025-07-15 20:57:12 +0530

    Add Jellyfish and Undertale [A] from Codeforces

--- 1875/A - Jellyfish and Undertale/solution.java
+++ 1875/A - Jellyfish and Undertale/solution.java
@@ -1,20 +1,22 @@
-import java.util.*;
+import java.util.Scanner;
  
-public class MishkatIT {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int t = sc.nextInt(); // Number of test cases
+ 
+        int t = sc.nextInt();  // Number of test cases
         while (t-- > 0) {
             long a = sc.nextLong();
             long b = sc.nextLong();
             int n = sc.nextInt();
  
             long ans = b;
+ 
             for (int i = 0; i < n; i++) {
                 long x = sc.nextLong();
                 ans += Math.min(a - 1, x);
             }
+ 
             System.out.println(ans);
         }
  
