9898af796c9b7a7d4f75356f4491f57f006e630b
Ayush Tiwari <90121562+ayushtiwari18@users.noreply.github.com>
2025-08-06 21:57:43 +0530

    Add Exciting Bets [A] from Codeforces

--- 1543/A - Exciting Bets/solution.java
+++ 1543/A - Exciting Bets/solution.java
@@ -1,10 +1,23 @@
-import java.util.*;
+import java.util.Scanner;
  
 public class Main {
+    public static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+ 
+    public static long power(long x, long y, long MOD) {
+        if (y == 0) return 1;
+        if (y % 2 == 0)
+            return power((x * x) % MOD, y / 2, MOD) % MOD;
+        else
+            return (x * power((x * x) % MOD, (y - 1) / 2, MOD) % MOD) % MOD;
+    }
  
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();  // Number of test cases
+ 
         while (t-- > 0) {
             long a = sc.nextLong();
             long b = sc.nextLong();
@@ -13,9 +26,11 @@ public class Main {
                 System.out.println("0 0");
             } else {
                 long diff = Math.abs(a - b);
-                long minMoves = Math.min(a % diff, diff - (a % diff));
-                System.out.println(diff + " " + minMoves);
+                long rem = Math.min(a % diff, diff - a % diff);
+                System.out.println(diff + " " + rem);
             }
         }
+ 
+        sc.close();
     }
 }
